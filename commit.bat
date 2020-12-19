f29e90b3d0c365d619755be1615f0fdd7018ae30
ndnam198 <ndnam198@gmail.com>
2020-12-19 22:13:22 +0700

    [$timestamp]: $message

--- commit.bat
+++ commit.bat
@@ -4,7 +4,7 @@ timestamp=$(date +"%D %T")
 message=$1
 git add *
 git commit -m "[$timestamp]: $message"
-git push https://github.com/ndnam198/Virtual-Assistance.git master
+git push https://github.com/ndnam198/vitual_assistance-env.git master
 echo "*******************************************************"
 echo "Time commit: [$timestamp]"
 echo "Commit message: \"$message\""
